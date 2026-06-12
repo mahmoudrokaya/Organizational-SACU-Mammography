8e8dd46fb3385f89ecf90e367ac0f38f34d37236
Mahmou Rokaya <33787285+mahmoudrokaya@users.noreply.github.com>
2026-06-12 06:15:19 +0300

    Create __init__P.py for preprocessing utilities
    Add preprocessing module for Organizational-SACU-Mammography

---
+++ __init__P.py
@@ -0,0 +1,14 @@
+"""
+Preprocessing module for Organizational-SACU-Mammography.
+
+This package contains utilities and pipelines for:
+
+- DICOM preprocessing
+- Photometric normalization
+- Orientation correction
+- Breast-region extraction
+- Image quality control
+- Metadata preparation
+"""
+
+__all__ = []
